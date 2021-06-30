157754ea4da00c134b3a25d464658096fe79fbfe
Wes Young <wesley.young@du.edu>
2021-06-30 11:44:00 -0600

    Setup express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.listen(PORT, () => {
+  console.log(`API server now at port ${PORT}!`);
+});
